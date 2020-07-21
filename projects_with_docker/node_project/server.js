d2f46ea9e022461182d00bdcdb83524e57d9c369
Abhinav Singh <abhinav.foss@gmail.com>
2020-07-21 17:51:34 +0530

    Added the node project with docker

---
+++ projects_with_docker/node_project/server.js
@@ -0,0 +1,18 @@
+// Express is a minimal and flexible Node.js web application framework
+const express = require('express');
+
+// Constants for host and port
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// Creating the app
+const app = express();
+
+// Defining logic when user visits the root or home page
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Listening at the particular port
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
