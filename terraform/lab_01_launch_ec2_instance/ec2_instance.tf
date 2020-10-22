a6a0332edd7ecf1f852513607f3d296061d2ff94
Abhinav Singh <abhinav.foss@gmail.com>
2020-10-22 15:36:44 +0530

    Initial version of lab 1 in terraform

--- terraform/lab_01_launch_ec2_instance/ec2_instance.tf
+++ terraform/lab_01_launch_ec2_instance/ec2_instance.tf
@@ -1,4 +1,7 @@
 terraform {
+
+  /* The provider block configures the named provider, in our case aws, which is responsible for creating and managing resources. A provider is a plugin that Terraform uses to translate the API interactions with the service. A provider is responsible for understanding API interactions and exposing resources. Because Terraform can interact with any API, you can represent almost any infrastructure type as a resource in Terraform. 
+  */
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,12 +10,25 @@ terraform {
   }
 }
 
+/* 
+The provider block configures the named provider, in our case aws, which is responsible for creating and managing resources. A provider is a plugin that Terraform uses to translate the API interactions with the service. A provider is responsible for understanding API interactions and exposing resources. Because Terraform can interact with any API, you can represent almost any infrastructure type as a resource in Terraform.
+
+*/
+
 provider "aws" {
-  profile = "default"
+  profile = "default" # Profile name in the ~/.aws/credentials
   region  = "us-west-1"
 }
 
-resource "aws_instance" "ubuntu_18.04" {
-  ami = "ami-03fac5402e10ea93b"
+/*
+The resource block defines a piece of infrastructure. A resource might be a physical component such as an EC2 instance, or it can be a logical resource such as a Heroku application.
+*/
+
+resource "aws_instance" "ubuntu_1804" {
+  count         = 2
+  ami           = "ami-03fac5402e10ea93b"
   instance_type = "t2.micro"
+  tags = {
+    Name = "Ubuntu 1804"
+  }
 }
