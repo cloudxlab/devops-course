d54ab26dc44100c736def47c1b15821c6f0edd4a
Abhinav Singh <abhinav.foss@gmail.com>
2020-10-22 22:58:35 +0530

    Added lab 02 in terraform

---
+++ terraform/lab_02_launch_ec2_instance_variables/ec2_instance.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default" # Profile name in the ~/.aws/credentials
+  region  = var.region
+}
+
+resource "aws_instance" "ubuntu" {
+  count         = 1
+  ami           = var.amis[var.region]
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Ubuntu"
+  }
+}
---
+++ terraform/lab_02_launch_ec2_instance_variables/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-west-1"
+}
+
+variable "amis" {
+  type = "map"
+  default = {
+    "us-west-1" = "ami-03fac5402e10ea93b"
+    "us-west-2" = "ami-07a29e5e945228fa1"
+  }
+}
+
+output "ami" {
+  value = aws_instance.ubuntu.ami
+}
